47f7c6f85695dcbd62a9a03423cef7d3dfdfc3ac
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2020-08-22 10:15:26 +0530

    update for payout

---
+++ build/precache-manifest.6ae729bd1ddfe5a35bef4fa5cf2304e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86be9308457db88298f718412543f4c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fe357aeb1d7651b242",
+    "url": "/static/js/2.489e9eed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.489e9eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff38ab6af7865d019aee",
+    "url": "/static/js/main.4ce75d98.chunk.js"
+  },
+  {
+    "revision": "526fcba762c983c70dc4",
+    "url": "/static/js/runtime-main.e71c7b41.js"
+  }
+]);
